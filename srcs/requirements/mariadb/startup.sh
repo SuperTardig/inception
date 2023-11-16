607585a5a7502db2f572cab991faf807a1fcfc15
Benjamin Perron <bperron@c1r1p5.42quebec.com>
2023-11-16 15:02:48 -0500

    I hate this shit

--- srcs/requirements/mariadb/startup.sh
+++ srcs/requirements/mariadb/startup.sh
@@ -1,12 +1,12 @@
 #!bin/bash
 
-service mysqld start 
+service mysql start 
 
 mysql -u root -p$SQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -u root -p$SQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -u root -p$SQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEDGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -u root -p$SQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -u root -p$SQL_ROOT_PASSWORD -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mysql -u root -p$SQL_ROOT_PASSWORD -e "FLUSH PRIVILEDGES;"
-#mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+mysql -u root -p$SQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
+mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 
 exec mysqld_safe
