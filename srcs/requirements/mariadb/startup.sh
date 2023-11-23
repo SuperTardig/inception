eb6957f637d3912724deae747f7c130eb3d5f12d
Benjamin Perron <bperron@c1r1p5.42quebec.com>
2023-11-23 14:44:51 -0500

    going crazy, mariadb works when building alone but not in compose

--- srcs/requirements/mariadb/startup.sh
+++ srcs/requirements/mariadb/startup.sh
@@ -2,21 +2,25 @@
 
 service mysql start
 
+sleep 1
 while true; do
-	if mysqladmin ping -h localhost -u root -p$SQL_ROOT_PASSWORD; then
+	if mysqladmin ping -h localhost -u root -p${SQL_ROOT_PASSWORD} --silent; then
 		break;
 	fi
 	sleep 1
 done
 
-
-mysql -u root -p$SQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mysql -u root -p$SQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -u root -p$SQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -u root -p$SQL_ROOT_PASSWORD -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mysql -u root -p$SQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
-
-mysqladmin -u root -p$SQL_ROOT_PASSWORD -S /var/run/myslqd/mysqld.sock shutdown
-
-
+sleep 1
+mysql -u root -p${SQL_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+sleep 1
+mysql -u root -p${SQL_ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+sleep 1
+mysql -u root -p${SQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+sleep 1
+mysql -u root -p${SQL_ROOT_PASSWORD} -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+sleep 1
+mysql -u root -p${SQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
+sleep 1
+mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
+sleep 1
 exec mysqld_safe
