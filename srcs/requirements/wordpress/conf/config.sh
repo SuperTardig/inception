e3e1a1239453c25d5fdede11ea9091ea339cbd44
Benjamin Perron <bperron@c1r1p5.42quebec.com>
2023-11-27 11:25:30 -0500

    for vm

--- srcs/requirements/wordpress/conf/config.sh
+++ srcs/requirements/wordpress/conf/config.sh
@@ -65,7 +65,7 @@ log_info() {
 # Attendre que MariaDB soit en cours d'exécution
 sleep 1
 log_info "Attente du démarrage de MariaDB..."
-while ! mysqladmin ping -h"$DB_HOST" -u"$DB_USER" -p"$DB_PASSWORD" --silent; do
+while ! mysqladmin ping -h"mariadb" -u"$SQL_USER" -p"$SQL_PASSWORD" --silent; do
   log_info "En attente du démarrage de MariaDB..."
   sleep 1
 done
@@ -74,7 +74,7 @@ log_success "MariaDB est en cours d'exécution."
 # Attendre que la base de données soit créée
 sleep 1
 log_info "Attente de la création de la base de données WordPress..."
-while ! mysql -h"$DB_HOST" -u"$DB_USER" -p"$DB_PASSWORD" -e "USE $DB_NAME" 2>/dev/null; do
+while ! mysql -h"mariadb" -u"$SQL_USER" -p"$SQL_PASSWORD" -e "USE $SQL_DATABASE" 2>/dev/null; do
   log_info "En attente de la création de la base de données WordPress..."
   sleep 1
 done
@@ -83,19 +83,19 @@ log_success "Base de données WordPress créée avec succès."
 # Créer la configuration WordPress
 sleep 1
 log_success "Création de la configuration WordPress..."
-wp config create --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASSWORD" --dbhost="$DB_HOST" --locale="ca_FR" --path="/var/www/wordpress"
+wp config create --dbname="$SQL_DATABASE" --dbuser="$SQL_USER" --dbpass="$SQL_PASSWORD" --dbhost="mariadb" --locale="ca_FR" --path="/var/www/wordpress"
 log_success "Configuration WordPress créée avec succès."
 
 # Installer WordPress
 sleep 1
 log_success "Installation de WordPress..."
-wp core install --url="$WP_DOMAIN" --title="$DB_NAME" --admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_EMAIL" --skip-email --path="/var/www/wordpress"
+wp core install --allow-root --url='https://bperron.42.fr' --title=title --admin_user=bperron --admin_password=$SQL_ROOT_PASSWORD	--admin_email=$EMAIL --path='var/www/wordpress'
 log_success "WordPress installé avec succès."
 
 # Créer un utilisateur WordPress
 sleep 1
 log_success "Création de l'utilisateur WordPress..."
-wp user create "$WP_USER" "$WP_USER_EMAIL" --role=author --user_pass="$WP_USER_PASSWORD" --path="/var/www/wordpress"
+wp user create --allow-root "$SQL_USER" "$EMAIL" --role=author --user_pass=$SQL_PASSWORD --path='/var/www/wordpress'
 log_success "Utilisateur WordPress créé avec succès."
 
 # Démarrer PHP-FPM
