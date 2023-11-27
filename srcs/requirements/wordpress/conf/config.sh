9ac5f8255c75e8100bb6be8138a68cfbd52a33c3
Benjamin Perron <bperron@c1r1p5.42quebec.com>
2023-11-27 15:06:43 -0500

    change debian from buster to bullseye and vm update

--- srcs/requirements/wordpress/conf/config.sh
+++ srcs/requirements/wordpress/conf/config.sh
@@ -1,105 +1,36 @@
 #!bin/bash
 
-#echo -e "starting script"
+sleep 30
 
-#sleep 1
-#while ! mysqladmin ping -h"$SQL_DATABASE" -u"$SQL_USER" -p"$SQL_PASSWORD" --silent; do
-#	echo -e "waiting for mariadb"
-#	sleep 1
-#done
-#echo -e "wait is done"
+if [ ! -e /tmp/done ]
+then
+	touch /tmp/done
+	wp config create --allow-root \
+	--dbname=$SQL_DATABASE \
+	--dbuser=$SQL_USER \
+	--dbpass=$SQL_PASSWORD \
+	--dbhost=mariadb:3306 \
+	--path='var/www/wordpress'
 
+	wp core install --allow-root \
+	--url='https://bperron.42.fr' \
+	--title=title \
+	--admin_user=bperron \
+	--admin_password=$SQL_ROOT_PASSWORD \
+	--admin_email=$EMAIL \
+	--path='var/www/wordpress'
 
-#if [ ! -e /tmp/done ]
-#then
-#	touch /tmp/done
-#	wp config create --allow-root \
-#	--dbname=$SQL_DATABASE \
-#	--dbuser=$SQL_USER \
-#	--dbpass=$SQL_PASSWORD \
-#	--dbhost=mariadb:3306 \
-#	--path='var/www/wordpress'
-#
-#	wp core install --allow-root \
-#	--url='https://bperron.42.fr' \
-#	--title=title \
-#	--admin_user=bperron \
-#	--admin_password=$SQL_ROOT_PASSWORD \
-#	--admin_email=$EMAIL \
-#	--path='var/www/wordpress'
-#
-#	wp user create --allow-root \
-#	$SQL_USER $EMAIL \
-#	--role=author \
-#	--user_pass=$SQL_PASSWORD \
-#	--path='/var/www/wordpress'
-#
-#	if [ ! -e /run/php ]
-#	then
-#		mkdir /run/php
-#	fi
-#	echo allo
-#fi
-#echo fankj
-#/usr/sbin/php-fpm7.3 -F
+	wp user create --allow-root \
+	$SQL_USER $EMAIL \
+	--role=author \
+	--user_pass=$SQL_PASSWORD \
+	--path='/var/www/wordpress'
 
+	if [ ! -e /run/php ]
+	then
+		mkdir /run/php
+	fi
+fi
+echo "launching php-fpm"
 
-
-#!/bin/sh
-
-# Fonction pour afficher un message en vert
-log_success() {
-  echo -e "\033[32m[ENTRY SCRIPT] $1\033[0m"  # Vert
-}
-
-# Fonction pour afficher un message en jaune
-log_wait() {
-  echo -e "\033[33m[ENTRY SCRIPT] $1\033[0m"  # Jaune
-}
-
-# Fonction pour afficher un message en bleu
-log_info() {
-  echo -e "\033[34m[ENTRY SCRIPT] $1\033[0m"  # Bleu
-}
-
-# Attendre que MariaDB soit en cours d'exécution
-sleep 10
-log_info "Attente du démarrage de MariaDB..."
-while ! mysqladmin ping -h"$DB_HOST" -u"$DB_USER" -p"$DB_PASSWORD" --silent; do
-  log_info "En attente du démarrage de MariaDB..."
-  sleep 1
-done
-log_success "MariaDB est en cours d'exécution."
-
-# Attendre que la base de données soit créée
-sleep 1
-log_info "Attente de la création de la base de données WordPress..."
-while ! mysql -h"$DB_HOST" -u"$DB_USER" -p"$DB_PASSWORD" -e "USE $DB_NAME" 2>/dev/null; do
-  log_info "En attente de la création de la base de données WordPress..."
-  sleep 1
-done
-log_success "Base de données WordPress créée avec succès."
-
-# Créer la configuration WordPress
-sleep 1
-log_success "Création de la configuration WordPress..."
-wp config create --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASSWORD" --dbhost="$DB_HOST" --locale="ca_FR" --path="/var/www/wordpress"
-log_success "Configuration WordPress créée avec succès."
-
-# Installer WordPress
-sleep 1
-log_success "Installation de WordPress..."
-wp core install --url="$WP_DOMAIN" --title="$DB_NAME" --admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_EMAIL" --skip-email --path="/var/www/wordpress"
-log_success "WordPress installé avec succès."
-
-# Créer un utilisateur WordPress
-sleep 1
-log_success "Création de l'utilisateur WordPress..."
-wp user create "$WP_USER" "$WP_USER_EMAIL" --role=author --user_pass="$WP_USER_PASSWORD" --path="/var/www/wordpress"
-log_success "Utilisateur WordPress créé avec succès."
-
-# Démarrer PHP-FPM
-sleep 1
-log_info "Démarrage de PHP-FPM..."
-php-fpm81 --nodaemonize
-log_success "PHP-FPM est démarré avec succès."
+exec php-fpm7.3 -F
